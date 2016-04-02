b9713911b43789476e037e4ad8c707bb24bdbbc1
Marcelin Da Cruz <projets@marcelin.pro>
2016-04-02 10:14:57 +0200

    Endpoint

---
+++ htdocs/index.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-# enable debugging
+import cgitb
+cgitb.enable()
+print("Content-Type: text/html;charset=utf-8")
+print()
+print("<h1 style='color:rgb(0,123,167);'>Hello World!</h1>")
