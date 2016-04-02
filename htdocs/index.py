019c8f1b1db373d851c4476f0000414658cd8a98
Marcelin Da Cruz <marcelin.da.cruz@gmail.com>
2016-04-02 10:40:14 +0200

    Endpoint

--- htdocs/index.py
+++ htdocs/index.py
@@ -2,6 +2,11 @@
 # -*- coding: UTF-8 -*-# enable debugging
 import cgitb
 cgitb.enable()
+root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(root+"/app/")
 print("Content-Type: text/html;charset=utf-8")
 print()
 print("<h1 style='color:rgb(0,123,167);'>Hello World!</h1>")
+print(root)
+
+
